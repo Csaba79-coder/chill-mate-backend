692a9c3a3a0df6485a183839f0bb94dbf42058fe
Csaba Vadász <csabavadasz79@gmail.com>
2025-03-14 09:27:37 +0100

    Initial commit

---
+++ src/main/java/com/csaba79coder/chillmatebackend/ChillMateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.csaba79coder.chillmatebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChillMateBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChillMateBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/csaba79coder/chillmatebackend/ChillMateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csaba79coder.chillmatebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChillMateBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
